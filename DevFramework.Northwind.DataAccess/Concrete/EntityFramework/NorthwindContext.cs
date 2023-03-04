ca117f5e890cf8bad25a9b87b896f082d2826912
Kamil Deniz <kamil07deniz@gmail.com>
2023-03-04 20:34:18 +0300

    Mapping

--- DevFramework.Northwind.DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DevFramework.Northwind.DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using DevFramework.Northwind.DataAccess.Concrete.EntityFramework.Mappings;
+using DevFramework.Northwind.Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Text;
 
 namespace DevFramework.Northwind.DataAccess.Concrete.EntityFramework
 {
-    internal class NorthwindContext
+    internal class NorthwindContext : DbContext
     {
+        public NorthwindContext()
+        {
+            Database.SetInitializer<NorthwindContext>(null);
+        }
+        public DbSet<Product> Products { get; set; }
+
+        protected override  void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new ProductMap());
+        }
     }
 }
