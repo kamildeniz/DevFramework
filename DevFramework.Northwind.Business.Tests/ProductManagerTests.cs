38ed4bf707e616719d1e48ce13d9fd460467b487
Kamil Deniz <kamil07deniz@gmail.com>
2023-03-08 06:00:26 +0300

    Message

---
+++ DevFramework.Northwind.Business.Tests/ProductManagerTests.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+namespace DevFramework.Northwind.Business.Tests
+{
+    [TestClass]
+    public class ProductManagerTests
+    {
+        [ExpectedExeption(typeof(ValidationExeption))]
+        [TestMethod]
+        public void Product_validation_check()
+        {
+            Mock<IProductDal> mock = new Mock<IProductDal>();
+            ProductManager productManager = new ProductManager(mock.Object);
+
+            productManager.Add(new Product());
+        }
+    }
+}
