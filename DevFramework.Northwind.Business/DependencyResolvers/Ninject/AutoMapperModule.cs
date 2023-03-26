0bd1e572f52fb5dbdc2d7a4a1654b6e6895c7b02
Kamil Deniz <kamil07deniz@gmail.com>
2023-03-27 02:34:30 +0300

    message

--- DevFramework.Northwind.Business/DependencyResolvers/Ninject/AutoMapperModule.cs
+++ DevFramework.Northwind.Business/DependencyResolvers/Ninject/AutoMapperModule.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AutoMapper;
+using Ninject.Modules;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,19 @@ using System.Threading.Tasks;
 
 namespace DevFramework.Northwind.Business.DependencyResolvers.Ninject
 {
-    internal class AutoMapperModule
+    public class AutoMapperModule : NinjectModule
     {
+        public override void Load()
+        {
+            Bind<IMapper>().ToConstant(CreateConfiguration().CreateMapper());
+        }
+        private MapperConfiguration CreateConfiguration()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfiles(GetType().Assembly);
+            });
+            return config;
+        }
     }
 }
