bfd2ef3fe87a3ac415b81b3487844b21f414109e
Kamil Deniz <kamil07deniz@gmail.com>
2023-04-01 17:53:36 +0300

    Message

---
+++ DevFramework.Core/Utilities/Common/WcfProxy.cs
@@ -0,0 +1,24 @@
+﻿using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.ServiceModel;
+
+namespace DevFramework.Core.Utilities.Common
+{
+    public class WcfProxy<T>
+    {
+        public static T CreateChannel()
+        {
+            string baseAdress = ConfigurationManager.AppSettings["ServiceAdress"];
+            string adress = string.Format(baseAdress, typeof(T).Name.Substring(1));
+
+            var binding = new BasicHttpBinding();
+            var channel = new ChannelFactory<T>(binding,adress);
+            return channel.CreateChannel();
+        }
+    }
+}
